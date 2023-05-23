1c7c8a6ae0402e2499aec5f718ddf422d6fb64b0
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2023-05-22 19:19:41 -0600

    L8: Handling Loading and Error States

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -16,8 +16,15 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data } = useQuery("posts", fetchPosts);
-  if (!data) return <p>Loading...</p>;
+  const { data, error, isError, isLoading } = useQuery("posts", fetchPosts);
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>Oops, Something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
