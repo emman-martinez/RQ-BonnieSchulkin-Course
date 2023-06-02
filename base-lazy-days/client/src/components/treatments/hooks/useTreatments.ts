bbcbd640c273bc20532d0bc9cfa0b7cd8cdbd441
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2023-06-02 14:24:25 -0600

    L41: onError Handler for useQuery

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,8 +12,15 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
+  const toast = useCustomToast();
   const fallback = [] as Treatment[];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    onError: (error) => {
+      const title =
+        error instanceof Error ? error.message : 'Error connecting to server';
+      toast({ title, status: 'error' });
+    },
+  });
 
   return data;
 }
